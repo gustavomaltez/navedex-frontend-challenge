dc18a5cfbf1503292a7d48f67e42692d6102f2a9
Gustavo Maltez <gustavo.maltez@hotmail.com>
2021-03-03 17:00:41 -0300

    style: added Login form styles

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -2,11 +2,14 @@ import React, { useRef } from 'react';
 import { SubmitHandler, FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import Input from '../../components/Input';
+import logoImg from '../../assets/images/logo.svg';
+import { Container } from './styles';
 
 interface FormData {
   name: string;
   email: string;
 }
+
 const Login: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
@@ -15,10 +18,14 @@ const Login: React.FC = () => {
   };
 
   return (
-    <Form ref={formRef} onSubmit={handleSubmit}>
-      <Input name="name" label="E-mail" />
-      <Input name="email" label="Senha" />
-    </Form>
+    <Container>
+      <Form ref={formRef} onSubmit={handleSubmit}>
+        <img src={logoImg} alt="Logo" />
+        <Input name="name" label="E-mail" />
+        <Input name="email" label="Senha" />
+        <button type="submit">Entrar</button>
+      </Form>
+    </Container>
   );
 };
 export default Login;
--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -1,3 +1,23 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    border: 1px solid #212121;
+    padding: 1.5rem;
+    min-width: 450px;
+    min-height: 400px;
+
+    img {
+      width: 230px;
+      height: 60px;
+    }
+  }
+`;
