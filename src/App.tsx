b341d846695f9a8cd21ad784fc93c936dd0e2984
Gustavo Maltez <gustavo.maltez@hotmail.com>
2021-03-03 16:35:21 -0300

    feat: added styled-components, global styles and Montserrat font

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import GlobalStyle from './styles/global';
 
 const App: React.FC = () => (
-  <div className="App">
-    <p>Hello Nave!</p>
-  </div>
+  <>
+    <GlobalStyle />
+  </>
 );
 
 export default App;
