b0b41e6f8f27362dc12ea3515b18290caaf5e2dd
Gustavo Maltez <gustavo.maltez@hotmail.com>
2021-03-03 17:35:47 -0300

    feat: added auth hook

---
+++ src/hooks/auth.tsx
@@ -0,0 +1,72 @@
+import React, { createContext, useCallback, useContext, useState } from 'react';
+import api from '../services/api';
+
+interface User {
+  email: string;
+  id: string;
+}
+
+interface AuthState {
+  token: string;
+  user: User;
+}
+
+interface SignInCredentials {
+  email: string;
+  password: string;
+}
+
+interface AuthContextData {
+  user: User;
+  signIn(credentials: SignInCredentials): Promise<void>;
+  signOut(): void;
+}
+
+const AuthContext = createContext<AuthContextData>({} as AuthContextData);
+
+const AuthProvider: React.FC = ({ children }) => {
+  const [data, setData] = useState<AuthState>(() => {
+    const token = localStorage.getItem('@NaveDex:token');
+    const user = localStorage.getItem('@NaveDex:user');
+
+    if (token && user) {
+      return { token, user: JSON.parse(user) };
+    }
+
+    return {} as AuthState;
+  });
+
+  const signIn = useCallback(async ({ email, password }) => {
+    const response = await api.post('sessions', { email, password });
+
+    const { token, user } = response.data;
+    localStorage.setItem('@NaveDex:token', token);
+    localStorage.setItem('@NaveDex:user', JSON.stringify(user));
+    setData({ token, user });
+  }, []);
+
+  const signOut = useCallback(() => {
+    localStorage.removeItem('@NaveDex:token');
+    localStorage.removeItem('@NaveDex:user');
+
+    setData({} as AuthState);
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+function useAuth(): AuthContextData {
+  const context = useContext(AuthContext);
+
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+
+  return context;
+}
+
+export { AuthProvider, useAuth };
