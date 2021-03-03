7214dade5de63018aa8647197b61818d9e729424
Gustavo Maltez <gustavo.maltez@hotmail.com>
2021-03-03 16:50:25 -0300

    feat: added unform and input component

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
-import { Container } from './styles';
+import React, { useRef } from 'react';
+import { SubmitHandler, FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
+import Input from '../../components/Input';
 
+interface FormData {
+  name: string;
+  email: string;
+}
 const Login: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  const handleSubmit: SubmitHandler<FormData> = data => {
+    console.log(formRef);
+  };
+
   return (
-    <Container>
-      <div>
-        <img src="" alt="" />
-        <button type="button">Entrar</button>
-      </div>
-    </Container>
+    <Form ref={formRef} onSubmit={handleSubmit}>
+      <Input name="name" label="E-mail" />
+      <Input name="email" label="Senha" />
+    </Form>
   );
 };
-
 export default Login;
