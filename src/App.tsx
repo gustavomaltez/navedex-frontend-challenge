6819033dd050aa85bfb1b457218f5494158ad221
Gustavo Maltez <gustavo.maltez@hotmail.com>
2021-03-03 17:36:50 -0300

    feat: added AppProvider in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Login from './pages/Login';
 import GlobalStyle from './styles/global';
+import AppProvider from './hooks';
 
 const App: React.FC = () => (
-  <>
+  <AppProvider>
     <Login />
     <GlobalStyle />
-  </>
+  </AppProvider>
 );
 
 export default App;
