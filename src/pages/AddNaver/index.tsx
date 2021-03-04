91993ce230d6035606d782a70cfc9eee394a95bf
Gustavo Maltez <gustavo.maltez@hotmail.com>
2021-03-03 21:05:10 -0300

    feat: added AddNaver and EditNaver pages

---
+++ src/pages/AddNaver/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../../components/Header';
+import { Container } from './styles';
+
+const AddNaver: React.FC = () => {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
+};
+
+export default AddNaver;
---
+++ src/pages/EditNaver/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../../components/Header';
+import { Container } from './styles';
+
+const EditNaver: React.FC = () => {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
+};
+
+export default EditNaver;
